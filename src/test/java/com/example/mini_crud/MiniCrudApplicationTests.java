4141d7d449442827f9831005a39b5648d0d1da36
leonel_the13 <vicor32leonel@gmail.com>
2026-04-08 10:35:45 +0100

    mini-curd Masgo

---
+++ src/test/java/com/example/mini_crud/MiniCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
